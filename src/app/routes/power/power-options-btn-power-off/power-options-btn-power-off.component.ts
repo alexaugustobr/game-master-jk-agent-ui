72be27dfaf9d1a1bd74d548e0febaf8946975664
Alex <alex.silva@algaworks.com.br>
2021-06-24 21:06:35 -0300

    power options

--- src/app/routes/power/power-options-btn-power-off/power-options-btn-power-off.component.ts
+++ src/app/routes/power/power-options-btn-power-off/power-options-btn-power-off.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ErrorHandlerService } from 'app/core/error-handler.service';
+import { ModalComponent } from 'app/shared/modal/modal.component';
+import { ModalConfig } from 'app/shared/modal/modal.config';
+import { ToastService } from 'app/shared/toast/toast.service';
+import { PowerService } from '../power.service';
 
 @Component({
   selector: 'app-power-options-btn-power-off',
@@ -6,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./power-options-btn-power-off.component.css']
 })
 export class PowerOptionsBtnPowerOffComponent implements OnInit {
+  
+  @ViewChild('modal') private modalComponent!: ModalComponent;
+  modalConfig : ModalConfig = {
+    modalTitle: "Shutdown the server",
+    dismissButtonLabel: "No",
+    confirmButtonLabel: "Yes",
+    shouldConfirm: () => true,
+    shouldDismiss: () => true,
+    onConfirm: async () => {
+      return await this.onCommand()
+    },
+    onDismiss:() => true,
+    disableConfirmButton: () => false,
+    disableDismissButton: () => false,
+    hideConfirmButton: () => false,
+    hideDismissButton: () => false,
+  }
 
-  constructor() { }
+  modalMessage = "Are you sure that you want shutdown the server?"
+  
+  constructor(private errorHandlerService: ErrorHandlerService,
+              private powerService: PowerService,
+              private toastService: ToastService,) { }
 
   ngOnInit(): void {
+  }
+
+  async openModal() {
+    return await this.modalComponent.open()
+  }
+
+  async onCommand() {
+    await this.powerService.poweroff()
+        .then(()=> this.toastService.showSuccess("Server is turning off"))
+        .catch((error)=> this.errorHandlerService.handle(error));
+    return true
   }
 
 }
--- src/app/routes/power/power-options-btn-power-on/power-options-btn-power-on.component.ts
+++ src/app/routes/power/power-options-btn-power-on/power-options-btn-power-on.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ErrorHandlerService } from 'app/core/error-handler.service';
+import { ModalComponent } from 'app/shared/modal/modal.component';
+import { ModalConfig } from 'app/shared/modal/modal.config';
+import { ToastService } from 'app/shared/toast/toast.service';
+import { PowerService } from '../power.service';
 
 @Component({
   selector: 'app-power-options-btn-power-on',
@@ -6,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./power-options-btn-power-on.component.css']
 })
 export class PowerOptionsBtnPowerOnComponent implements OnInit {
+  
+  @ViewChild('modal') private modalComponent!: ModalComponent;
+  modalConfig : ModalConfig = {
+    modalTitle: "Turn on the server",
+    dismissButtonLabel: "No",
+    confirmButtonLabel: "Yes",
+    shouldConfirm: () => true,
+    shouldDismiss: () => true,
+    onConfirm: async () => {
+      return await this.onCommand()
+    },
+    onDismiss:() => true,
+    disableConfirmButton: () => false,
+    disableDismissButton: () => false,
+    hideConfirmButton: () => false,
+    hideDismissButton: () => false,
+  }
 
-  constructor() { }
+  modalMessage = "Are you sure that you want turn on the server?"
+  
+  constructor(private errorHandlerService: ErrorHandlerService,
+              private powerService: PowerService,
+              private toastService: ToastService,) { }
 
   ngOnInit(): void {
+  }
+
+  async openModal() {
+    return await this.modalComponent.open()
+  }
+
+  async onCommand() {
+    await this.powerService.poweroff()
+        .then(()=> this.toastService.showSuccess("Server is turning on"))
+        .catch((error)=> this.errorHandlerService.handle(error));
+    return true
   }
 
 }
