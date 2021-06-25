a391ab8ae0684994acb1eccec2b86c401cd1b90c
Alex <alex.silva@algaworks.com.br>
2021-06-24 21:51:40 -0300

    docker file e prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com',
+  apiUrl: 'http://159.69.214.101:8080',
 
   tokenAllowedDomains: [ new RegExp('algamoney-api.herokuapp.com') ],
   tokenDisallowedRoutes: [ new RegExp('\/oauth\/token') ]
