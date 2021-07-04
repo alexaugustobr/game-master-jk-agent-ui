a7933a95a31a35ed5e993710a673e6441f21a2f3
Alex <alex.silva@algaworks.com.br>
2021-07-04 20:12:48 -0300

    game file up

--- src/app/routes/file/game-file-upload/game-file-upload.component.ts
+++ src/app/routes/file/game-file-upload/game-file-upload.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ErrorHandlerService } from 'app/core/error-handler.service';
+import { ModalComponent } from 'app/shared/modal/modal.component';
+import { ModalConfig } from 'app/shared/modal/modal.config';
+import { ToastService } from 'app/shared/toast/toast.service';
+import { GameFileService } from '../game-file.service';
 
 @Component({
   selector: 'app-game-file-upload',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameFileUploadComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('modal') private modalComponent!: ModalComponent;
+  @ViewChild('file') private fileInput: ElementRef;
+
+  modalConfig : ModalConfig = {
+    modalTitle: "Upload Map",
+    dismissButtonLabel: "Cancel",
+    confirmButtonLabel: "Upload",
+    shouldConfirm: () => true,
+    shouldDismiss: () => true,
+    onConfirm: async () => {
+      return await this.onUpload()
+    },
+    onDismiss:() => true,
+    disableConfirmButton: () => !this.file || this.isUploading,
+    disableDismissButton: () => false,
+    hideConfirmButton: () => false,
+    hideDismissButton: () => false,
+  }
+  
+  file: File;
+  progress = 0;
+  isUploading = false;
+
+  constructor(
+    private gameFileService : GameFileService,
+    private toastService: ToastService,
+    private errorHandlerService: ErrorHandlerService
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  async openGameFileUploadModal() {
+    return await this.modalComponent.open()
+  }
+
+  handleFileInput(files: FileList) {
+    this.file = files.item(0);
+  }
+
+  async onUpload() {
+    this.isUploading = true;
+    await this.gameFileService.upload(this.file)
+    .toPromise()
+    .then(resp => this.toastService.showSuccess('Upload completed.'))
+    .catch((err)=>{
+      this.errorHandlerService.handle(err);
+    })
+    this.file = null;
+    this.fileInput.nativeElement.value = "";
+    this.isUploading = false;
+    return true
   }
 
 }
