6300707d53e703f6b6ec1ee6dbc3db7927d254b7
Alex <alex.silva@algaworks.com.br>
2021-09-25 12:53:05 -0300

    envs

--- src/environments/environment.tfs-lab.ts
+++ src/environments/environment.tfs-lab.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://tfs-barn-api.chozat.com',
+  apiUrl: 'http://tfs-lab-api.chozat.com',
 
   tokenAllowedDomains: [ new RegExp('algamoney-api.herokuapp.com') ],
   tokenDisallowedRoutes: [ new RegExp('\/oauth\/token') ]
